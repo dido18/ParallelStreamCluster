36c3b8d1c0fa5a8e0d066f80755a510b8678d6b1
spm1505 <spm1505@r720-phi.itc.unipi.it>
2016-01-29 20:01:55 +0100

    I have added makefile for xeon phi machine

--- src/ff_stream_cluster.cpp
+++ src/ff_stream_cluster.cpp
@@ -2,7 +2,7 @@
 //All rights reserved.
 
 //Redistribution and use in source and binary forms, with or withou
-//modification, are permitted provided that the following conditions are
+//modification, are permitted provided that the following conditions ar
 //    * Redistributions of source code must retain the above copyright
 //      notice, this list of conditions and the following disclaimer
 //    * Redistributions in binary form must reproduce the above copyright
@@ -10,7 +10,8 @@
 //      documentation and/or other materials provided with the distribution.
 //    * Neither the name of Princeton University nor the
 //      names of its contributors may be used to endorse or promote products
-//      derived from this software without specific prior written permission.
+//      derived from this software without specific prior written permissi
+
 
 //THIS SOFTWARE IS PROVIDED BY PRINCETON UNIVERSITY ``AS IS'' AND ANY
 //EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
